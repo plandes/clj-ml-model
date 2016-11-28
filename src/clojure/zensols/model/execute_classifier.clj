804131541268690f4ef5dd55c34ba5cc1cb861a3
Paul Landes <landes@mailc.net>
2016-11-28 11:30:41 -0600

    moving combined feature creation to feature repo

--- src/clojure/zensols/model/execute_classifier.clj
+++ src/clojure/zensols/model/execute_classifier.clj
@@ -93,44 +93,6 @@ docs](https://github.com/plandes/clj-ml-model)."
     (->> ((:feature-metas-fn model-conf))
          (into {}))))
 
-(def ^{:dynamic true :private true} combined-features nil)
-
-(defmacro with-combined-features
-  {:style/indent 0}
-  [& forms]
-  `(binding [combined-features (atom {})]
-     (do ~@forms)))
-
-(defn combine-feature-vals
-  "Apply feature creation functions only where they don't already exist
-  in [[combined-features]]."
-  [fmap]
-  (let [cfeats (if combined-features @combined-features)
-        fmap (->> fmap
-                  (map (fn [[fkey form]]
-                          (let [feats (or (and cfeats (get cfeats fkey))
-                                          ((eval form)))]
-                            {fkey feats})))
-                  (apply merge))]
-    (when combined-features
-      (log/tracef "combined: %s" (pr-str @combined-features))
-      (swap! combined-features merge fmap))
-    (->> fmap vals (apply merge))))
-
-(defmacro combine-features
-  "Combine features from the forms for which features don't already exist in
-  **features**, which is a map with functions as keys and features already
-  created with that function as the value of the respective function."
-  [& forms]
-  `(->> (merge ~@(->> (map (fn [form]
-                             (->> form first
-                                  (ns-resolve (ns-name *ns*))
-                                  var-get
-                                  (#(hash-map % (read-string (str "#" (pr-str form)))))))
-                           forms)
-                      doall))
-        combine-feature-vals))
-
 (defn- create-instances
   "Create the weka Instances object using sets of features created from
   **:create-feature-sets-fn**.  If **context** is given call :set-context-fn.
