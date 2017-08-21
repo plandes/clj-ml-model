d777cc6f3b668d47f35f64fdb3136d71e02c1019
Paul Landes <landes@mailc.net>
2017-08-21 10:42:21 -0500

    dep

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
 
                  ;; command line
-                 [com.zensols.tools/actioncli "0.0.16"]
+                 [com.zensols.tools/actioncli "0.0.17"]
 
                  ;; dev
                  [com.zensols.gui/tabres "0.0.6"]
